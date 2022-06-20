9127bcca2a06264e07cb96c5d07409e9b66a2d9b
CreatifyID <farelkanza05@gmail.com>
2022-06-20 21:14:21 +0700

    Add Tablet Version

--- public/js/app.js
+++ public/js/app.js
@@ -20,17 +20,15 @@ const about2 = document.getElementById("aboutbut2");
 const projects = document.getElementById("projectsbut");
 const contact = document.getElementById("contactbut");
 
+const homelg = document.getElementById("homebutlg");
+const aboutlg = document.getElementById("aboutbutlg");
+const projectslg = document.getElementById("projectsbutlg");
+const contactlg = document.getElementById("contactbutlg");
+
 const homepage = document.getElementById("body");
 const aboutpage = document.getElementById("about");
 const projectspage = document.getElementById("projects");
 const contactpage = document.getElementById("contact");
-
-function preventScroll(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  return false;
-}
 
 function navdown() {
   document.getElementById("navbar").style.top = "0";
@@ -71,6 +69,31 @@ contact.addEventListener("click", () => {
   document.getElementById("navpage").classList.toggle("-translate-x-full");
   document.getElementById("body").classList.toggle("overflow-y-hidden");
   contactpage.scrollIntoView(true);
-  window.scrollBy(0, -50);
+  window.scrollBy(0, -100);
+  setTimeout(navdown, 750);
+});
+
+homelg.addEventListener("click", () => {
+  document.getElementById("navpage").classList.toggle("-translate-x-full");
+  homepage.scrollIntoView(true);
+  window.scrollBy(0, -150);
+  setTimeout(navdown, 750);
+});
+aboutlg.addEventListener("click", () => {
+  document.getElementById("navpage").classList.toggle("-translate-x-full");
+  aboutpage.scrollIntoView(true);
+  window.scrollBy(0, -100);
+  setTimeout(navdown, 750);
+});
+projectslg.addEventListener("click", () => {
+  document.getElementById("navpage").classList.toggle("-translate-x-full");
+  projectspage.scrollIntoView(true);
+  window.scrollBy(0, -100);
+  setTimeout(navdown, 750);
+});
+contactlg.addEventListener("click", () => {
+  document.getElementById("navpage").classList.toggle("-translate-x-full");
+  contactpage.scrollIntoView(true);
+  window.scrollBy(0, -100);
   setTimeout(navdown, 750);
 });
